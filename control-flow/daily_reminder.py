4e4480b1324727c7e128bf339f55d5ecaa9d6daa
noel knowles <coalzcoley@gmail.com>
2025-07-20 07:32:35 +0300

    Add files via upload

---
+++ control-flow/daily_reminder.py
@@ -0,0 +1,28 @@
+task = input("Enter a task description: ")
+priority = input("What's the task's priority? ,(high/medium/low): ").lower()
+time_bound = input("Is the task time-bound? (yes or no): ")
+
+reminder_message = [f"'{task}' is a "]
+
+match priority:
+  case "high":
+    reminder_message.append("high priority task ")
+  case "medium":
+    reminder_message.append("medium priority task ")
+  case "low":
+    reminder_message.append("low priority task ")
+  case _:
+    reminder_message.append("task lacks priority")
+    print("Error: invalid priority")
+
+if time_bound == "yes":
+  reminder_message.append("that requires immediate attention today!")
+elif time_bound == "no" and priority == "low":
+  reminder_message.append(". Consider completing it when you have free time.")
+elif time_bound != "no":
+  print("Invalid time bound input!")
+
+if priority == "low" and time_bound == "no":
+  print(f"Note: {''.join(reminder_message)}")
+else:
+  print(f"Reminder: {''.join(reminder_message)}")
