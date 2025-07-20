202c53504a4288a8b09e60935483df3fbac16e51
noel knowles <coalzcoley@gmail.com>
2025-07-20 06:52:25 +0300

    Add files via upload

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a number to see its multiplication table: "))
+
+for i in range(1, 11):
+  product = number * 1
+  print(f"{number} * {1} = {product}")
+  print()
+  i += 1
