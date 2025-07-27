601058f3527b7d238543bad6b3194c7f9153b3ab
noel knowles <coalzcoley@gmail.com>
2025-07-27 11:01:42 +0300

    Add files via upload

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,14 @@
+FAHRENHEIT_TO_CELSIUS_FACTOR = (5/9)
+CELSIUS_TO_FAHRENHEIT_FACTOR = (9/5)
+
+def convert_to_celsius(farenheit):
+  global FAHRENHEIT_TO_CELSIUS_FACTOR
+  return FAHRENHEIT_TO_CELSIUS_FACTOR
+
+def convert_to_fahrenheit(celsius):
+  global CELSIUS_TO_FAHRENHEIT_FACTOR
+  return CELSIUS_TO_FAHRENHEIT_FACTOR
+
+temperature =int(input("Enter the temperature (Celsius or Farenheit): "))
+if temperature != range(1,11):
+  print("Error: Invalid Temperature. Please enter a numeric value")
