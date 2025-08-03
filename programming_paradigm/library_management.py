f37c8962710526de9586e7d24f0fe20e6ae868bf
noel knowles <coalzcoley@gmail.com>
2025-08-03 10:44:48 +0300

    Update library_management.py

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -21,3 +21,35 @@ class Book:
 
     def __str__(self):
         return f"{self.title} by {self.author}"
+
+class Library:
+    def __init__(self):
+        self._books = [] 
+
+    def add_book(self, book):
+        if isinstance(book, Book):
+            self._books.append(book)
+        else:
+            raise TypeError("Only instances of Book can be added.")
+
+    def check_out_book(self, title):
+        for book in self._books:
+            if book.title == title and book.is_available():
+                book.check_out()
+                return f'"{title}" has been checked out.'
+        return f'"{title}" is not available.'
+
+    def return_book(self, title):
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+                return f'"{title}" has been returned.'
+        return f'"{title}" is not currently checked out.'
+
+    def list_available_books(self):
+        available = [str(book) for book in self._books if book.is_available()]
+        if available:
+            return "\n".join(available)
+        else:
+            return "No books available."
+
