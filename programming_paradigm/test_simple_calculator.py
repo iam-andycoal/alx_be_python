eb6653e6a4517b6d99b755d1ba0257bd6b97862d
noel knowles <coalzcoley@gmail.com>
2025-08-03 10:32:24 +0300

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,15 +4,14 @@ from simple_calculator import SimpleCalculator
 class TestSimpleCalculator(unittest.TestCase):
 
     def setUp(self):
-        """Set up a SimpleCalculator instance before each test."""
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(3, 4), 7)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(0, 0), 0)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10, 5), 5)
         self.assertEqual(self.calc.subtract(0, 10), -10)
         self.assertEqual(self.calc.subtract(-5, -5), 0)
@@ -32,3 +31,4 @@ class TestSimpleCalculator(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
