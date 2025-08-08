d19ae65547d07edd10108dc1cad963209e96e761
noel knowles <coalzcoley@gmail.com>
2025-08-08 07:53:51 +0300

    Add files via upload

---
+++ oop/main.py
@@ -0,0 +1,17 @@
+from book_class import Book
+
+def main():
+    # Creating an instance of Book
+    my_book = Book("Opening Moves", "Steven James", 1980)
+
+    # Demonstrating the __str__ method
+    print(my_book)  # Expected to use __str__
+
+    # Demonstrating the __repr__ method
+    print(repr(my_book))  # Expected to use __repr__
+
+    # Deleting a book instance to trigger __del__
+    del my_book
+
+if __name__ == "__main__":
+    main()
